11fa1bf02bcdc2fd88885e377fa54728d1b94840
Dawson <hellochen19970916@gmail.com>
2018-09-19 10:48:42 -0700

    Add some test functions

--- attendance/gradescope.py
+++ attendance/gradescope.py
@@ -24,6 +24,9 @@ def main ():
 
     header['Cookie'] = '_ga=GA1.2.1141250143.1534819808; signed_token=b2F3VlB0RVJtN1VucUhVUHZWcXFyZEt5SjdVYUdOU21qVHlITStidXpKTT0tLUxnY200U2c5MnMybVJzbVFMM3BUYXc9PQ%3D%3D--836f6c0f9343a41d90e28a9d138a42e0a719c295; _gid=GA1.2.157058492.1537208283; _gradescope_session=L0JNemtFdmZ4MnpzRVhjL3BVSndON1pIUzBKWkc2cDJqeUxlUkJ2ZDBVOWZjeHBBSWpRZjFrMXJyeE5CdWNhdDRUcDAxaHVBQUhTWlo4OUdYOStNSXRoN0pwcDZTSVVQcUZSYjJ6Mzhyemdpd2tja0REZ2k2cnp6WkFPZGp4Qm42Nmx5b2VucDJHTUUyb3AyckxXYUNYRE8rN1Jhb3A4RlZ0RU9oSzdHZzJHQThMa3pOSExiVndWNm1NdU5GaVJqZXlBSVZZQ2Fzb0xSYm5rOHlsMFRlSWg0TXM0dTR0UStjaWw3T2QvbWcxcE9tRjhHTWV1QldSRHZZUEQrbEViaFZXVjMwT0dPK29wNU8vRjFSVGJGdjZnVVlzeHpDeStPK3c5TUhucWxzci9ESlViWSsyQ2k1MkdRcHdjRFFQUnhMVEVSSnNPR1NpdjExNlZXVUJZU0hwL3V2OWpyZ0VXSWQwUnc3SXF2TDI0PS0tb0pVWWdoU3g3cmx3SXRaQSsrRzdlZz09--aedf694a963a41ea333d137f153d59beea1d3741'
 
+    if not check_attendance(url, header):
+        # test if the connect is right
+        print('connect failed')
     while True:
         if check_attendance(url, header, check):
             print(time.asctime(time.localtime(time.time())) ,' Success find the',check)
@@ -45,7 +48,7 @@ def pop_notification():
 
 
 
-def check_attendance(url, header, check):
+def check_attendance(url, header, check = 'Attendance 1'):
     s = requests.session()
     re = s.get(url, headers = header)
     #cookie = cookiejar.CookieJar()
